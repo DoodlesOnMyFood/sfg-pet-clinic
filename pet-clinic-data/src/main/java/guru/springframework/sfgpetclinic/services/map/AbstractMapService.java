b2b67a37a4b43ca24b4f08135e303e7afb72f7a6
jisoo <fyimbtmn@gmail.com>
2021-04-09 18:24:55 +0900

    Added Spring DI to data loader. Closes #21

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,9 +1,11 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
 
 public abstract class AbstractMapService<T, ID> {
 	protected Map<ID, T> map = new HashMap<>();
