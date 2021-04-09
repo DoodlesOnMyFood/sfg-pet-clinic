01af6510f3bc6bef7498ab81b7c8ddb84aec0d9f
jisoo <fyimbtmn@gmail.com>
2021-04-09 19:16:26 +0900

    Refactored Map impl to auto increment id. Closes #22

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
 		owner1.setFirstName("ABC");
 		owner1.setLastName("QWE");
 
 		ownerService.save(owner1);
 
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
 		owner2.setFirstName("123");
 		owner2.setLastName("456");
 
@@ -38,13 +36,11 @@ public class DataLoader implements CommandLineRunner {
 		System.out.println("Loaded Owners....");
 
 		Vet vet1 = new Vet();
-		vet1.setId(1L);
 		vet1.setFirstName("Vet?");
 		vet1.setLastName("1?");
 		vetService.save(vet1);
 
 		Vet vet2 = new Vet();
-		vet2.setId(2L);
 		vet2.setFirstName("Vet");
 		vet2.setLastName("2");
 		vetService.save(vet2);
