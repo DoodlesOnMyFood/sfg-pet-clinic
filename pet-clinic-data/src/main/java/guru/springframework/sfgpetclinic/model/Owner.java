56789a02cf41cb1f72edb295607607b459b57adb
jisoo <fyimbtmn@gmail.com>
2021-04-11 19:02:25 +0900

    Added properties to Owners. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -3,7 +3,34 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person{
+	private String Address;
+	private String City;
+	private String telephone;
 	private Set<Pet> pets;
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public String getCity() {
+		return City;
+	}
+
+	public void setCity(String city) {
+		City = city;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 
 	public Set<Pet> getPets() {
 		return pets;
